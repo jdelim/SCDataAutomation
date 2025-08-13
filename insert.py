b50e1d48ec5a0b65fa3f98a6288aeb07653e644d
Jadriel <jadrieldelim@gmail.com>
2025-08-12 22:54:04 -0700

    finish create tsv headers method

--- insert.py
+++ insert.py
@@ -1,10 +1,14 @@
 import csv
+import pandas as pd
+import os
+import traceback
+
 COL_1, COL_2, COL_3 = "EVENT_ID", "SESSION_ID", "AGE"
 COL_4, COL_5, COL_6 = "GRADE", "ORG_ID", "GENDER_ID"
 COL_7, COL_8, COL_9 = "ETHNICITY_ID", "STUDENT_CODE", "POSTAL_CODE"
 COL_10, COL_11, COL_12 = "IS_RETURNING_STUDENT_FLAG", "STUDENT_FIRST_NAME", "STUDENT_LAST_NAME"
 
-def readCSV(csv_file_path):
+def readCSV(csv_file_path: str):
     try:
         with open(csv_file_path, mode='r', newline='') as file:
             csv_reader = csv.reader(file)
@@ -41,6 +45,28 @@ def clean_gender(rows):
         data.append(new_row)
     return data
 
+def create_tsv_with_headers(file_path: str) -> bool:
+    headers = [
+        COL_1, COL_2, COL_3,
+        COL_4, COL_5, COL_6,
+        COL_7, COL_8, COL_9,
+        COL_10, COL_11, COL_12
+    ]
+    try:
+        df = pd.DataFrame(columns=headers)
+        df.to_csv(file_path, sep="\t", index=False)
+
+        if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+            return True
+        else:
+            print(f"Error! File {file_path} was not created or is empty!")
+            return False
+    except Exception as e:
+        print(f"Error! Failed to create TSV file {file_path}: {e}")
+        print("TRACEBACK:")
+        print(traceback.format_exc)
+        return False
+
 def pretty_print(data):
     # Convert all values to strings for consistent formatting
     str_rows = [[str(val) for val in row] for row in data]
@@ -57,12 +83,10 @@ def pretty_print(data):
     # Join all rows with newline characters and return as one string
     return "\n".join(lines)
 
-    
-        
-
 def main():
-    my_data = readCSV('C:/Users/Jadriel delim/Documents/SCDataAutomation/data/Uncommon_Goods_Student_Demographics.csv')
-    print(pretty_print(clean_gender(my_data)))
+    #my_data = readCSV('data/Uncommon_Goods_Student_Demographics.csv')
+    #print(pretty_print(clean_gender(my_data)))
+    create_tsv_with_headers('data/test1.tsv')
 
 if __name__ == "__main__":
     main()
